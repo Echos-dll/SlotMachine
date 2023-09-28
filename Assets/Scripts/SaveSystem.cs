a47103f8fbc2df44ee5af922beb787338066b06e
Ekrem <ekremibram@outlook.com>
2023-09-28 20:53:02 +0300

    Slot machine refactored. RandomResultGenerator.cs added. SaveSystem.cs added.

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public static void Save(SaveData saveData)
+    {
+        string data = JsonUtility.ToJson(saveData);
+        PlayerPrefs.SetString("SaveData", data);
+    }
+
+    public static bool TryLoad(out SaveData dataCount)
+    {
+        dataCount = new SaveData();
+
+        if (!PlayerPrefs.HasKey("SaveData")) return false;
+        
+        SaveData load = JsonUtility.FromJson<SaveData>(PlayerPrefs.GetString("SaveData"));
+        dataCount._pocketData = load._pocketData;
+        dataCount._pickedCountData = load._pickedCountData;
+        dataCount._pickedAmountsData = load._pickedAmountsData;
+        return true;
+    }
+}
+
+[Serializable]
+public struct SaveData
+{
+    [JsonProperty("pcd")] public int _pickedCountData;
+    [JsonProperty("pd")] public float[] _pocketData;
+    [JsonProperty("pa")] public int[] _pickedAmountsData;
+}
--- Assets/Scripts/SlotMachine.cs
+++ Assets/Scripts/SlotMachine.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
-using Newtonsoft.Json;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -10,15 +8,18 @@ public class SlotMachine : MonoBehaviour
     [SerializeField] private List<SpinningSlot> _slots;
     [SerializeField] private List<SlotItem> _slotItems;
     [SerializeField] private List<Result> _results;
+    [SerializeField] private ParticleSystem _coinParticleSystem;
+
+    [Header("Settings")]
     [SerializeField] private float _spinSpeed;
     [SerializeField] private bool _slowStop;
-    [SerializeField] private ParticleSystem _coinParticleSystem;
     
     [Header("Events")]
     [SerializeField] private ScriptableStateEvent _stateEvent;
     
     [Header("DEBUG MODE")]
     [SerializeField] private bool _debugMode;
+    [SerializeField] private bool _debugLoad;
     [SerializeField] private int _amountToPick;
     
     private float[] m_pocket;
@@ -32,10 +33,10 @@ public class SlotMachine : MonoBehaviour
     private int m_currentResult;
     private bool m_loaded;
 
+    private RandomResultGenerator m_resultGenerator;
+
     private void Awake()
     {
-        Load();
-        
         Texture2D[] slotTextures = new Texture2D[_slotItems.Count];
         Texture2D[] blurTextures = new Texture2D[_slotItems.Count];
 
@@ -47,33 +48,20 @@ public class SlotMachine : MonoBehaviour
         
         foreach (SpinningSlot slot in _slots)
             slot.SetTextures(slotTextures, blurTextures);
+
+        m_resultGenerator = new RandomResultGenerator(_results);
     }
 
     private void Start()
     {
         SetSlotItemValues();
-        FindLeastChance();
-        if (!m_loaded) FillPocket();
 
+        if (_debugLoad)
+            m_resultGenerator.DebugLoad();
+        
         if (_debugMode)
-        {
-            m_pocket = new float[_results.Count];
-            m_pickedAmounts = new int[_results.Count];
-            
-            m_pickedCount = 0;
-
-            FillPocket();
-            
-            for (int i = 0; i < _amountToPick; i++)
-            {
-                PickRandomResult();
-            }
-
-            for (int i = 0; i < m_pickedAmounts.Length; i++)
-            {
-                Debug.Log($"Index: {i} Picked " + m_pickedAmounts[i] + " times");
-            }
-        }
+            m_resultGenerator.DebugRandomResult(_amountToPick);
+        
     }
 
     private void SetSlotItemValues()
@@ -83,62 +71,6 @@ public class SlotMachine : MonoBehaviour
             SlotItem item = _slotItems[i];
             item.SetValue(1f / _slotItems.Count * (i - (int)(_slotItems.Count / 2 )));
         }
-    }
-
-    private void FillPocket()
-    {
-        m_remaining = m_pocket;
-
-        m_pocket = new float[_results.Count];
-        
-        for (int i = 0; i < m_pocket.Length; i++)
-        {
-            m_pocket[i] = _results[i]._chance / m_leastChance + m_remaining[i];
-        }
-    }
-
-    private int PickRandomResult()
-    {
-        m_totalWeight = 0;
-        
-        for (int i = 0; i < m_pocket.Length; i++)
-        {
-            m_totalWeight += m_pocket[i];
-        }
-        
-        float pick = Random.value * m_totalWeight;
-        int chosenIndex = 0;
-        float cumulativeWeight = m_pocket[0];
-        
-        while (pick > cumulativeWeight && chosenIndex < m_pocket.Length - 1)
-        {
-            chosenIndex++;
-            cumulativeWeight += m_pocket[chosenIndex];
-        }
-        
-        m_pocket[chosenIndex]--;
-        m_pickedAmounts[chosenIndex]++;
-        m_pickedCount++;
-        
-        if (m_pickedCount % (100 / m_leastChance) == 0)
-            FillPocket();
-
-        return chosenIndex;
-    }
-
-    private void FindLeastChance()
-    {
-        float least = 100;
-        
-        foreach (Result result in _results)
-        {
-            if (result._chance < least)
-            {
-                least = result._chance;
-            }
-        }
-        
-        m_leastChance = least;
     }
 
     private void StartSpinning()
@@ -167,7 +99,7 @@ public class SlotMachine : MonoBehaviour
 
     private void StopSpinning()
     {
-        m_currentResult = PickRandomResult();
+        m_currentResult = m_resultGenerator.PickRandomResult();
         
         for (int i = 0; i < _slots.Count; i++)
         {
@@ -193,14 +125,13 @@ public class SlotMachine : MonoBehaviour
 
         if (m_stopCount != _slots.Count) return;
         
-        //_resultEvent.Invoke();
         _stateEvent.Invoke(State.Result);
         m_stopCount = 0;
     }
 
     private void SkipSpinning()
     {
-        m_currentResult = PickRandomResult();
+        m_currentResult = m_resultGenerator.PickRandomResult();
         
         for (int i = 0; i < _slots.Count; i++)
         {
@@ -240,33 +171,6 @@ public class SlotMachine : MonoBehaviour
         }
     }
 
-    private void Save()
-    {
-        SaveData save = new SaveData
-        {
-            _pocketData = m_pocket,
-            _pickedCountData = m_pickedCount,
-            _pickedAmountsData = m_pickedAmounts
-        };
-
-        string data = JsonUtility.ToJson(save);
-        PlayerPrefs.SetString("SaveData", data);
-    }
-
-    private void Load()
-    {
-        m_pocket = new float[_results.Count];
-        m_pickedAmounts = new int[_results.Count];
-
-        if (!PlayerPrefs.HasKey("SaveData")) return;
-        
-        SaveData load = JsonUtility.FromJson<SaveData>(PlayerPrefs.GetString("SaveData"));
-        m_pocket = load._pocketData;
-        m_pickedCount = load._pickedCountData;
-        m_pickedAmounts = load._pickedAmountsData;
-        m_loaded = true;
-    }
-
     private void OnEnable()
     {
         _stateEvent.AddListener(StateHandler);
@@ -275,11 +179,6 @@ public class SlotMachine : MonoBehaviour
     private void OnDisable()
     {
         _stateEvent.RemoveListener(StateHandler);
-    }
-
-    private void OnDestroy()
-    {
-        Save();
     }
 }
 
@@ -290,12 +189,4 @@ public struct Result
     public float _chance;
     public int _reward;
     public int _particleCount;
-}
-
-[Serializable]
-public struct SaveData
-{
-    [JsonProperty("pcd")] public int _pickedCountData;
-    [JsonProperty("pd")] public float[] _pocketData;
-    [JsonProperty("pa")] public int[] _pickedAmountsData;
 }
